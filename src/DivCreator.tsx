413debd573205da6c1e3050bcb0ebe9036e4acba
Yogeshwar B. <byogeshwarreddy@gmail.com>
2025-07-23 16:13:49 +0530

    Divs added on image.

--- src/DivCreator.tsx
+++ src/DivCreator.tsx
@@ -1,25 +1,45 @@
+import { useEffect, useRef, useState } from 'react'
+
 export default function DivCreator({ imageUrl }: { imageUrl: string }) {
+  const imgRef = useRef<HTMLImageElement>(null)
   function r() {
     return Math.floor(Math.random() * 256)
   }
-
+  useEffect(() => {
+    console.log(imgRef.current?.getBoundingClientRect().height)
+    console.log(imgRef.current?.getBoundingClientRect().width)
+    gridDimensionsRef.current.gridRowHeight =
+      Number(imgRef.current?.getBoundingClientRect().height) / rowcount
+    gridDimensionsRef.current.gridColumnWidth =
+      Number(imgRef.current?.getBoundingClientRect().width) / colcount
+    console.log('got dimensions', gridDimensionsRef.current)
+    setIsGridReady(true)
+  })
   // const color = "rgb(" + r() + "," + r() + "," + r() + ")";
 
-  const rowcount = 10,
-    colcount = 20
+  const rowcount = 10
+  const colcount = 20
+
+  const gridDimensionsRef = useRef<{
+    gridRowHeight: number
+    gridColumnWidth: number
+  }>({ gridRowHeight: 0, gridColumnWidth: 0 })
+  const [isGridReady, setIsGridReady] = useState<boolean>(false)
   const divArr = []
   for (let i = 0; i < rowcount; i++) {
     let temp = []
     for (let j = 0; j < colcount; j++) {
       temp.push(
         <div
-          className='inline'
+          className={`inline`}
           style={{
-            backgroundColor: `rgb(${r()},${r()},${r()})`
+            backgroundColor: `rgb(${r()},${r()},${r()})`,
+            width: `${gridDimensionsRef.current.gridColumnWidth}px`,
+            height: `${gridDimensionsRef.current.gridRowHeight}px`
           }}
         >
-          <span className='mix-blend-difference' style={{ color: 'white' }}>
-            {` ${i}-${j} `}
+          <span className='mix-blend-difference ' style={{ color: 'white' }}>
+            {/* {` ${i}-${j} `} */}
           </span>
         </div>
       )
@@ -28,13 +48,26 @@ export default function DivCreator({ imageUrl }: { imageUrl: string }) {
   }
   console.log(imageUrl)
   return (
-    <div className='h-100 w-full relative'>
-      <div className='absolute top-0 left-0 opacity-70'>
-        {divArr.map((row) => {
-          return <div>{row}</div>
-        })}
-      </div>
-      <img className='h-100 shadow-lg' src={imageUrl} alt='UploadedImage' />
+    <div className='object-contain h-100 w-full relative'>
+      {isGridReady ? (
+        <div className='absolute top-0 left-0 opacity-70'>
+          {divArr.map((row, index) => {
+            return (
+              <div key={index} className='flex flex-row '>
+                {row}
+              </div>
+            )
+          })}
+        </div>
+      ) : (
+        <></>
+      )}
+      <img
+        ref={imgRef}
+        className='h-100 shadow-lg object-contain'
+        src={imageUrl}
+        alt='UploadedImage'
+      />
     </div>
   )
 }
